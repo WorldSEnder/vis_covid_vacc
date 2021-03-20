366f9333112dd4eec44af0d24d73b4d98224c2c1
Martin Molzer <ga65guy@mytum.de>
2021-03-20 18:19:15 +0100

    double size of hatching

--- draw_vis.py
+++ draw_vis.py
@@ -466,7 +466,7 @@ a {
     svg.append(style)
     hatch_pattern = ET.fromstring(
 R"""
-<pattern id="diagonalHatch" width="2" height="10" patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse">
+<pattern id="diagonalHatch" width="4" height="10" patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse">
   <line x1="0" y1="0" x2="0" y2="10" style="stroke:#444; stroke-width:1" />
 </pattern>
 """
